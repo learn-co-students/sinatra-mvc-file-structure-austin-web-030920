cb4975ac181290611e46d1a33e70269376a84d8c
Patrick Jones <patrickjones.pmj@gmail.com>
2020-04-01 14:41:33 -0500

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,13 @@
-class Model
+class Dog
+    attr_accessor :name, :age, :breed
+    @@all = []
+    def initialize(name, breed, age)
+      @name, @age, @breed = name, age, breed
 
+      @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
